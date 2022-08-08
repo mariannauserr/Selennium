07bdd2115820241446c1015dba9396d916c287d1
Mariya Negretskaya <marian.93@mail.ru>
2022-08-08 09:48:56 +0300

    Reformat code

--- src/test/java/ru/netology/web/FormTest.java
+++ src/test/java/ru/netology/web/FormTest.java
@@ -1,52 +1,55 @@
 
-    package ru.netology.web;
-    import org.junit.jupiter.api.AfterEach;
-    import org.junit.jupiter.api.BeforeAll;
-    import org.junit.jupiter.api.BeforeEach;
-    import org.junit.jupiter.api.Test;
-    import org.openqa.selenium.By;
-    import org.openqa.selenium.WebDriver;
-    import org.openqa.selenium.chrome.ChromeDriver;
+package ru.netology.web;
 
-    import io.github.bonigarcia.wdm.WebDriverManager;
-    import org.openqa.selenium.chrome.ChromeOptions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-    import static org.junit.jupiter.api.Assertions.assertEquals;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-
-    public class FormTest {
-        private WebDriver driver;
-
-        @BeforeAll
-        public static void setUpAll() {
-            WebDriverManager.chromedriver().setup();
-        }
-            @BeforeEach
-            public void setUp() {
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--disable-dev-shm-usage");
-                options.addArguments("--no-sandbox");
-                options.addArguments("--headless");
-                driver = new ChromeDriver(options);
-                driver.get("http://localhost:9999");
-            }
-            @AfterEach
-            public void tearDown() {
-                driver.quit();
-                driver = null;
-            }
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-        @Test
-        void orderFormTest() {
-            driver.get("http://localhost:9999");
-            driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов-Сидоров");
-            driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79996666666");
-            driver.findElement(By.className("checkbox__box")).click();
-            driver.findElement(By.className("button_view_extra")).click();
-            String text = driver.findElement(By.className("paragraph")).getText();
+public class FormTest {
+    private WebDriver driver;
 
-            assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
-        }
+    @BeforeAll
+    public static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
     }
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+    @Test
+    void orderFormTest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Иванов-Сидоров");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79996666666");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
 
